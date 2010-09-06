ad4dd626404a25154e0589aad1cbdd9a1f66d493
Troy Will <troydwill@gmail.com>
2010-09-06 14:21:11 -0700

    minor

--- 020_controller_welcome.sh
+++ 020_controller_welcome.sh
@@ -1,5 +1,5 @@
 source ./tdw-lib.sh
-${SCRIPT_DIR}/generate controller welcome index login logout view1 view2
+${SCRIPT_DIR}/generate controller welcome index login logout admin view2
 ${EDITOR} ${TOP_DIR}/config/routes.rb
-rm -i ${TOP_DIR}/public/index.html
-
+mv --interactive --verbose ${TOP_DIR}/public/index.html ${TOP_DIR}/public/index.html.original
+cp --interactive --verbose index.html ${TOP_DIR}/public/
--- rails-server.sh
+++ rails-server.sh
@@ -4,6 +4,6 @@ PORT=3001
 mongrel_rails stop
 rm log/mongrel.pid
 # TDW 2010-09-06: For reasons I don't understand I was getting ActionController::InvalidAuthenticityToken errors, and redirect errors when I started an application with mongrel_rails directly.
-COMMAND="ruby script/server -p $PORT -d"
+COMMAND="ruby script/server --port $PORT --daemon"
 echo $COMMAND
 $COMMAND
