4ea07cb31e1574a143a6275fcb471b0173dddad2
Troy Will <troydwill@gmail.com>
2010-09-07 17:05:21 -0700

    minor

---
+++ json/jsoninfo_controller.rb
@@ -0,0 +1,11 @@
+class JsoninfoController < ApplicationController
+  def customer
+    @customer = User.find(params[:id])
+    respond_to do |format|
+      format.json { render :layout => false , :json => @customer.to_json }
+    end
+  end
+end
+
+
+
