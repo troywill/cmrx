e1f0f0c167821f8c7e5d2eff65991a7e1dc13c4d
Troy Will <troydwill@gmail.com>
2010-09-11 16:04:23 -0700

    minor

---
+++ rest/authenticate_with_curl.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+BASE_URL='http://192.168.1.5:3000'
+CONTROLLER='welcome/login'
+HEADER_FILE='_fashionrx.header'
+COOKIE_FILE='cookie_file'
+RECORD_URL='http://192.168.1.5:3000/users/1'
+
+authenticate () {
+    curl --data 'register_email=troydwill@gmail.com&password=passwd1' \
+	--dump-header ${HEADER_FILE} \
+	${BASE_URL}/${CONTROLLER}
+}
+
+write_cookie () {
+    grep ookie ${HEADER_FILE} > ${COOKIE_FILE}
+}
+
+retrieve_record () {
+    curl --cookie ${COOKIE_FILE} ${RECORD_URL}
+}
+
+clear_password () {
+    curl --data 'register_email=troydwill@gmail.com&password=passwd1'
+}
+
+authenticate
+write_cookie
+retrieve_record
--- rest/authentication.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-URL='http://192.168.1.5:3000/welcome/login'
-curl --data 'register_email=troydwill@gmail.com&password=passwd1' \
-    --dump-header _fashionrx.header \
-    ${URL}
