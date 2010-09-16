b33d51487d2a9d9831ee817fd6afb5829dd29f5c
Troy Will <troydwill@gmail.com>
2010-09-16 11:48:21 -0700

    minor

--- demos/register_user.pl
+++ demos/register_user.pl
@@ -4,27 +4,31 @@ use strict;
 use JSON;
 
 my $server = 'http://troywill.com:4000';
-my $iphone_id = 'HG:A4:UI:90';
-my $register_ip = '0.0.0.0';
+my $register_ipv6 = '::ffff:192.0.2.128';
+my $ios_device_identifier = 'HG:A4:UI:90';
+my $ios_device_type_id = '1';
+my $hips = 39;
+my $bust = 39;
+my $waist = 26;
+my $height = 66;
 
 my %user = (
 	      user => {
-		       register_email => 'troydwill@gmail.com',
-		       register_ip => $register_ip,
-		       iphone_id => $iphone_id,
-		       hips => 39,
-		       bust => 39,
-		       waist => 26,
-		       height => 66
+		  register_email => 'troydwill@gmail.com',
+		  register_ipv6 => $register_ipv6,
+		  ios_device_indentifier => $ios_device_identifier,
+		  ios_device_type_id => $ios_device_type_id,
+		  hips => $hips,
+		  bust => $bust,
+		  waist => $waist,
+		  height => $height
     }
     );
 
 
 while ( 1 ) {
   &menu;
-  my $input = <STDIN> ;
-  chomp $input;
-  print "==> $input\n";
+  chomp( my $input = <STDIN> );
   &print_json_hash(\%user) if $input eq '1';
   &register_user if $input eq '2';
   last if $input eq 'q';
@@ -39,15 +43,6 @@ sub print_json_hash {
 }
  
 sub register_user {
-  print "Email address? "; chomp ( my $email_address = <STDIN> );
-  print "Waist measurement in inches? "; chomp ( my $waist = <STDIN> );
-  my %Hash = (
-	      user => {
-		       register_email => $email_address,
-		       waist => $waist
-		      }
-	     );
-
   my $json_text = encode_json \%user;
   my $curl_post_command = "curl --header 'Content-Type:application/json' --header 'Accept:application/json' --data '$json_text' $server/users 2>/dev/null";
   print "==> $curl_post_command\n\n";
@@ -59,8 +54,8 @@ sub register_user {
 
 sub menu {
     print "\n\n+-------------- Prescription RX Menu --------------+\n";
-    print "| Q to quit\n";
-  print "| 1. Print user hash in JSON\n";
-  print "| 2. Register a new user\n";
+    print "|\tQ to quit\n";
+  print "| 1. Print current User hash in JSON\n";
+  print "| 2. Register a new User (POST JSON User hash to $server/users)\n";
   print "| Choice? ";
 }
