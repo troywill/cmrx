8a38495e2cb5a56dba00c86d336a8dca6953ce8e
Troy Will <troydwill@gmail.com>
2010-09-21 16:51:55 -0700

    minor

--- clients/user-add-flash-curl.sh
+++ clients/user-add-flash-curl.sh
@@ -2,10 +2,11 @@
 # Example program to add a new user RESTfully with curl POSTing of XML data
 # 2010-09-20 troydwill@gmail.com
 
-EMAIL='foo@bar.com'
-FIRST_NAME='Betty'
-LAST_NAME='White'
+EMAIL='troydwill@gmail.com.com'
+FIRST_NAME='Troy'
+LAST_NAME='Will'
 HOST='http://stylerx.ath.cx'
+HOST='http://troywill.com:3002'
 API_KEY='784da8554937a966ec97aec8ed1ec659e25e334b'
 URL="${HOST}/users.xml?api_key=${API_KEY}"
 XML="<?xml version=\"1.0\" encoding=\"UTF-8\"?><user><email>${EMAIL}</email><first-name>${FIRST_NAME}</first-name><last-name>${LAST_NAME}</last-name></user>"
