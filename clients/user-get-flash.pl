56a2506963ece76110c97de511ad4a42d0096de2
Troy Will <troydwill@gmail.com>
2010-09-21 20:45:03 -0700

    minor

--- clients/user-get-flash.pl
+++ clients/user-get-flash.pl
@@ -7,13 +7,43 @@ use JSON;
 my $host = 'http://stylerx.ath.cx';
 my $iphone_api_key = '784da8554937a966ec97aec8ed1ec659e25e334b';
 my $flash_api_key = '4356e1973d1cf09a450d2704afc35d0e5be2304e';
-my $url = "${host}/users/1.json?api_key=${$flash_api_key}";
+my $url = "${host}/users/2.json?api_key=${flash_api_key}";
 
-my $ua = LWP::UserAgent->new();
-my $body = $ua->get($url);
-print keys%{$body};
-print "==> ${$body}{_msg}\n";
-print "==> ${$body}{_content}\n";
+my $user = &get_user;
+print ${$user}{'user'}{'email'}, "\n";
+&show_user_schema;
+
+sub get_user {
+    my $ua = LWP::UserAgent->new();
+    my $body = $ua->get($url);
+    # print keys%{$body};
+    # print "==> ${$body}{_msg}\n";
+    # print "==> ${$body}{_content}\n";
+    my $perl_scalar = from_json($body->decoded_content);
+    # print "Keys: ", keys %{$perl_scalar}, "\n";
+    # print ${$perl_scalar}{'email'}{'first_name'}, "\n";
+    return $perl_scalar;
+}
+
+sub show_user_schema {
+    
+    print <<HERE;
+  create_table "users", :force => true do |t|
+    t.string   "first_name",                :limit => 100, :default => ""
+    t.string   "last_name",                 :limit => 100, :default => ""
+    t.string   "email",                     :limit => 100
+    t.string   "api_key",                   :limit => 40,  :default => ""
+    t.string   "security_key",              :limit => 40,  :default => ""
+    t.string   "crypted_password",          :limit => 40
+    t.string   "salt",                      :limit => 40
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "remember_token",            :limit => 40
+    t.datetime "remember_token_expires_at"
+  end
+HERE
+}
+__END__
 
 exit;
 # $ua->default_header("HTTP_REFERER" => 'localhost' );
@@ -65,3 +95,74 @@ for my $key (keys %{$hash_ref}) {
 #    headerkey2 => value2, 
 # 		       );
 		       
+__END__
+# This file is auto-generated from the current state of the database. Instead of editing this file, 
+# please use the migrations feature of Active Record to incrementally modify your database, and
+# then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your database schema. If you need
+# to create the application database on another system, you should be using db:schema:load, not running
+# all the migrations from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 3) do
+
+  create_table "prescriptions", :force => true do |t|
+    t.integer  "user_id",            :null => false
+    t.integer  "created_by_id"
+    t.datetime "request_datetime"
+    t.datetime "available_datetime"
+    t.integer  "height_inches"
+    t.integer  "bust_inches"
+    t.integer  "waist_inches"
+    t.integer  "hip_inches"
+    t.boolean  "red_spots"
+    t.boolean  "dark_circles"
+    t.boolean  "wrinkles"
+    t.boolean  "freckles"
+    t.boolean  "loves_skin"
+    t.boolean  "other_skin_issues"
+    t.integer  "comfort_in_heels"
+    t.integer  "skin_tone"
+    t.integer  "hair_color"
+    t.string   "product_id"
+    t.string   "transaction_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "roles", :force => true do |t|
+    t.string   "name",              :limit => 40
+    t.string   "authorizable_type", :limit => 40
+    t.integer  "authorizable_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "roles_users", :id => false, :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "role_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "first_name",                :limit => 100, :default => ""
+    t.string   "last_name",                 :limit => 100, :default => ""
+    t.string   "email",                     :limit => 100
+    t.string   "api_key",                   :limit => 40,  :default => ""
+    t.string   "security_key",              :limit => 40,  :default => ""
+    t.string   "crypted_password",          :limit => 40
+    t.string   "salt",                      :limit => 40
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "remember_token",            :limit => 40
+    t.datetime "remember_token_expires_at"
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+
+end
+    
