0b3f9522e5f6eb145454048215f2af113fefa76b
Troy Will <troydwill@gmail.com>
2010-09-21 17:14:21 -0700

    minor

--- clients/user-add-flash-curl.sh
+++ clients/user-add-flash-curl.sh
@@ -2,15 +2,21 @@
 # Example program to add a new user RESTfully with curl POSTing of XML data
 # 2010-09-20 troydwill@gmail.com
 
-EMAIL='troydwill@gmail.com.com'
-FIRST_NAME='Troy'
-LAST_NAME='Will'
+echo "Email?" && read EMAIL
+echo "First name?" && read FIRST_NAME
+echo "Last name?" && read LAST_NAME='Will'
 HOST='http://stylerx.ath.cx'
-HOST='http://troywill.com:3002'
-API_KEY='784da8554937a966ec97aec8ed1ec659e25e334b'
-URL="${HOST}/users.xml?api_key=${API_KEY}"
-XML="<?xml version=\"1.0\" encoding=\"UTF-8\"?><user><email>${EMAIL}</email><first-name>${FIRST_NAME}</first-name><last-name>${LAST_NAME}</last-name></user>"
-
+HOST='http://troywill.com:3001'
+FLASH_APP_API_KEY='b1b5bf7b7e8c99e0b8b720d4b17004833dc69cb5'
+URL="${HOST}/users.xml?api_key=${FLASH_APP_API_KEY}"
+XML="
+<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<user>
+  <email>${EMAIL}</email>
+  <first-name>${FIRST_NAME}</first-name>
+  <last-name>${LAST_NAME}</last-name>
+</user>
+"
 curl --header 'Content-Type:application/xml' \
     --header 'Accept:application/xml' \
     --request POST \
